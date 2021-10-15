6d8481058b1452f47e4f7fd157c98f41706ed4f1
ya.shchanov <y.shchanov@gmail.com>
2021-10-15 21:52:30 +0300

    Migrate to PG to use heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2021_10_13_104107) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cached_tree_views", force: :cascade do |t|
     t.string "ancestry"
-    t.string "text"
+    t.string "text", null: false
     t.integer "state", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
